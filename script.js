9825de3b92c7ba38189a5786d6577014c9bbad3b
EmmaKespo <kespoemma@gmail.com>
2026-02-02 12:11:27 -0600

    update fizzbuzz game

---
+++ script.js
@@ -0,0 +1,12 @@
+let answer = parseInt(prompt("please enter the number you will like to fizzbuzz"));
+for (let i = 1; i <  answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i)
+    }
+}
